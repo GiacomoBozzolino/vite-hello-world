c833f63d642de4fb55e6d7e1ea75877fcf4ea6a5
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-06-19 16:16:22 +0200

    lavoro Bonus

---
+++ Bonus/src/App.vue
@@ -0,0 +1,26 @@
+<script >
+import AppTitle from './components/AppTitle.vue'
+import Jumbotron from './components/Jumbotron.vue'
+
+export default {
+  components:{
+    AppTitle,
+    Jumbotron,
+  }
+}
+</script>
+
+<template lang="">
+  <div>
+    <Jumbotron/>
+  </div>
+  <div>
+    <AppTitle/>
+  </div>
+</template>
+
+
+
+<style lang="">
+  
+</style>
---
+++ Bonus/src/components/Jumbotron.vue
@@ -0,0 +1,17 @@
+<script>
+    export default {
+        
+    }
+</script>
+
+<template lang="">
+    <div>
+        <img class="jumbo" src="../assets/vue.svg" alt="">
+    </div>
+</template>
+
+<style>
+.jumbo {
+    width:300px;
+}
+</style>
