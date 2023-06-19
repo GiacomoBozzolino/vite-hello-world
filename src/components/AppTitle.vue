befabf47e2ed08c9282cde1e390b4bfa9d23badc
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-06-19 15:56:44 +0200

    [FEAT] creazione nuovo componente

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+        <script>
+        
+        export default {
+            components: {
+                
+            }
+        }
+        </script>
+
+
+<template lang="">
+    <div>
+        <h1>La mia prima app con Vite!</h1>
+    </div>
+</template>
+
+<style lang="">
+    
+</style>
